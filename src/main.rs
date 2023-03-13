44abe2e135d8d47f0d7c0fd3f533dcb073789c70
Award <awardmalisi@gmail.com>
2023-03-13 01:10:26 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+async fn apples() -> impl Responder {
+    HttpResponse::Ok().body("Where are all of the apples, people?")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/apples").route("/award", web::get().to(apples)))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+//Request handler -
+//A handler, or request handler, is a program that receives a client request for access to the service, and translates the request into a form that the service can understand.
