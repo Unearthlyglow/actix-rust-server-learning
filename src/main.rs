782b9a146e3390504aac48563627d5ea3fcfdf68
Award <awardmalisi@gmail.com>
2023-03-15 22:49:08 -0500

    Added functionality to allow serving of index.html w/ index.css

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,46 @@
 //Video Idea :: Forest in SVHS
 
 // ---------------
+//Actix / Networking Specific Info ::
+
+//01 -- Use App::service for the handlers using routing macros and App::route for manually routed handlers, declaring the path and method.
+
+//02 -- Request handler - A handler, or request handler, is a program that receives a client request for access to the service, and translates the request into a form that the service can understand.
+
+//03 -- Extrctors??
+
+//04 -- TLS??
+
+//05 --
+
+//06 --
+
+//07 --
+
+//08 --
+
+//05 --
+
+//06 --
+
+//07 --
+
+//08 --
+
+//05 --
+
+//06 --
+
+//07 --
+
+//08 --
+
+//----
 
 use actix_files::Files;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde_json::json;
 use std::sync::Mutex;
 
 struct AppState {
@@ -26,9 +63,9 @@ struct AppStateWithCounter {
 }
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
+async fn app_name(data: web::Data<AppState>) -> String {
     let app_name = &data.app_name; // <- get app_name
-    format!("Hello {app_name}!") // <- response with app_name
+    format!("Hello My Friend this is {app_name}!") // <- response with app_name
 }
 
 #[get("/hello")]
@@ -41,6 +78,7 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+//This is an example of manual routing
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -61,34 +99,69 @@ async fn shared_state(data: web::Data<AppStateWithCounter>) -> String {
     format!("Request number: {counter}") // <- response with count
 }
 
+async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Book Store",
+        "books":[
+            {
+                "name":"Harry Potter",
+                "author":"J K Rowlings",
+                "image_path":"/static/image/download.jpeg"
+            },
+            {
+                "name":"Lord of the ring",
+                "author":"Tolken",
+                "image_path": "/static/image/lord_of.jpeg"
+            },
+            {
+                "name":"Americanah",
+                "author":"Chimamada Adichie",
+                "image_path":"/static/image/americanah.jpeg"
+            },
+            {
+                "name":"Elon Musk",
+                "author":"#####",
+                "image_path":"/static/image/elon.jpeg"
+            },
+        ]
+
+
+    });
+
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
+}
+
 #[actix_web::main]
+
 async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
     let counter = web::Data::new(AppStateWithCounter {
         counter: Mutex::new(0),
     });
 
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
     HttpServer::new(move || {
         App::new()
+            .service(Files::new("/static", "static").show_files_listing())
+            .app_data(handlebars_ref.clone())
+            .route("/", web::get().to(index))
             .app_data(counter.clone()) // <- register the created data
             .route("/sharedstate", web::get().to(shared_state))
             .app_data(web::Data::new(AppState {
                 app_name: String::from("Actix Web"),
             }))
-            .service(index)
+            .service(app_name) //example of a use of a routing macro with the .service() method. See line 34 for the function.
             .service(web::scope("/apples").route("/award", web::get().to(apples))) //example of using Actix's Scope method :: An application's scope acts as a namespace for all routes, i.e. all routes for a specific application scope have the same url path prefix.
             .service(hello)
-            .service(echo)
+            //example of a manual route with the .route() method.
             .route("/hey", web::get().to(manual_hello))
-            .service(Files::new("/static", "./static").show_files_listing())
-            .route("/index.html", web::get().to(home))
     })
     .bind(("127.0.0.1", 8090))?
     .run()
     .await
 }
-
-//The service() method -
-
-//Request handler -
-//A handler, or request handler, is a program that receives a client request for access to the service, and translates the request into a form that the service can understand.
